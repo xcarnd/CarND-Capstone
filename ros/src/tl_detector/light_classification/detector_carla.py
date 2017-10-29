19c019c061a7b85985830612981490a901650aea
Chen, Zhuhui <ekschencn@gmail.com>
2017-10-30 01:59:48 +0800

    Updated detector_carla model and weights.
    Hope this could be a more accurate model and weights.

--- ros/src/tl_detector/light_classification/detector_carla.py
+++ ros/src/tl_detector/light_classification/detector_carla.py
@@ -8,16 +8,19 @@ from keras.models import load_model
 import math
 import random
 
-input_size = (70, 192)
+input_size = (251, 684)
 
 def get_model(weights):
     inputs = Input(input_size + (3,))
     x = Lambda(lambda x : (x / 255.0) - 0.5)(inputs)
     x = Conv2D(12, (5, 5), strides = (2, 2), activation='relu', padding='SAME')(x)
+    x = MaxPooling2D()(x)
     x = Conv2D(24, (5, 5), strides = (2, 2), activation='relu', padding='SAME')(x)
+    x = MaxPooling2D()(x)
     x = Conv2D(36, (5, 5), strides = (2, 2), activation='relu', padding='SAME')(x)
+    x = MaxPooling2D()(x)
     x = Conv2D(48, (3, 3), activation='relu', padding='SAME')(x)
-    x = Conv2D(48, (3, 3), activation='relu', padding='SAME')(x)
+    x = MaxPooling2D()(x)
     x = Dropout(0.5)(x)
     x = Flatten()(x)
     x = Dense(100, activation='relu')(x)
@@ -26,7 +29,7 @@ def get_model(weights):
     x = Dropout(0.5)(x)
     x = Dense(10)(x)
     pred = Dense(3, activation='softmax')(x)
-
+    
     model = Model(inputs, pred)
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
